919078e9e5f1487354f82227c23bf710e8a544a1
PanicAthe <ekdekdgkrp20@gmail.com>
2023-07-17 16:38:57 +0900

    23.07.17

---
+++ Solved_Bronze/2884_알람 시계.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int h, m;
+	cin >> h >> m;
+	if (m >= 45) {
+		m -= 45;
+	}
+	else {
+		if (h > 0)
+		{
+			h -- ; m = m + 60 - 45;
+		}
+		else {
+			if (h == 0)
+			{
+				h = 23; m = m + 60 - 45;
+			}
+		}
+	}
+	cout << h << " " << m;
+}

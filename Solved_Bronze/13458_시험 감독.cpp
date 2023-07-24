6b4f88fc838bc18d5e86a57f9d1b90f454b61eff
PanicAthe <ekdekdgkrp20@gmail.com>
2023-07-24 16:33:28 +0900

    23.07.24

--- Solved_Bronze/13458_시험 감독.cpp
+++ Solved_Bronze/13458_시험 감독.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 int main() {
-	double n, in, b, c, s;
+	long long n, in, b, c, s;
 	s = 0;
 	cin >> n;
-	vector<double> a;
+	vector<long long> a;
 	for (int i = 0; i < n; i++) {
 		cin >> in;
 		a.push_back(in);
 	}
 	cin >> b >> c;
 	for (int i = 0; i < n; i++) {
-		s += ceil((a.at(i) - b) / static_cast<double>(c)) + 1;
+		s += 1;
+		if (a.at(i) - b > 0) {
+			if ((a.at(i) - b) % c == 0)
+				s += (a.at(i) - b) / c ;
+			else
+				s += (a.at(i) - b) / c + 1;
+		}
 	}
 	cout << s;
 }

b56ab95b9403ff52374256256386c4244e9f67af
PanicAthe <ekdekdgkrp20@gmail.com>
2023-09-07 17:02:23 +0900

    23.09.07

--- Solved_Silver/10610_30.cpp
+++ Solved_Silver/10610_30.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 using namespace std;
 
-int main() {
+int main() {  // 더 계산을 줄이는 방법도 있음
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string in;
+    cin >> in;
+    long long sum = 0;   //여기 실수
+    int n = in.length();
+
+    for (int i = 0; i < n; i++) {
+        sum += in[i] - '0';
+    }
+
+    sort(in.begin(), in.end(), greater<>());
+
+    if (in[n - 1] == '0' && sum % 3 == 0)
+        cout << in;
+    else
+        cout << -1;
 
 }

a07befad7602e53a2dae109030906033b7ccf7bb
PanicAthe <ekdekdgkrp20@gmail.com>
2023-09-15 21:18:35 +0900

    23.09.15

--- Solved_Silver/10610_30.cpp
+++ Solved_Silver/10610_30.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-int main() {  // 더 계산을 줄이는 방법도 있음
+int main() { 
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -11,16 +11,16 @@ int main() {  // 더 계산을 줄이는 방법도 있음
 
     string in;
     cin >> in;
-    long long sum = 0;   //여기 실수
-    int n = in.length();
+    long long sum = 0;   // 여기 실수 필수 (입력은 최대 10^5개의 숫자로 구성되어 있음)
+    int n = in.length();  
 
     for (int i = 0; i < n; i++) {
-        sum += in[i] - '0';
+        sum += in[i] - '0';  // 합을 계산
     }
 
-    sort(in.begin(), in.end(), greater<>());
+    sort(in.begin(), in.end(), greater<>()); // 받은 숫자를 내림차순으로 정렬
 
-    if (in[n - 1] == '0' && sum % 3 == 0)
+    if (in[n - 1] == '0' && sum % 3 == 0) // 맨 끝이 0이고 합이 3으로 나눠지면 출력
         cout << in;
     else
         cout << -1;
